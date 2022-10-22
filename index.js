9110d2abcd3362a0d9f1a1fa7366b7e5c733eb6e
gamachexx <31936427+olgam4@users.noreply.github.com>
2022-10-22 05:28:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cyclic = require('cyclic-dynamodb')
-const db = cyclic('stormy-cyan-hareCyclicDB')
+const db = new cyclic('stormy-cyan-hareCyclicDB')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
